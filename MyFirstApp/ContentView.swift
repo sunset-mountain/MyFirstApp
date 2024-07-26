932afaecc1a2c233fca57e0b75d2df0d82d1616a
samhitamattipalli <samattipalli@gmail.com>
2024-07-26 11:52:25 -0400

    Initial Commit

---
+++ MyFirstApp/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  MyFirstApp
+//
+//  Created by Samhita Mattipalli on 7/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Samhita Mattipalli on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
